1cbbcc9b9052e34e8ee14bcc95c9119a6e141275
Catriona Morrison <catriona.morrison@bbc.co.uk>
2021-09-29 20:27:40 +0100

    Update tab header
    - add footer tabheader
    - add in accessibility for buttons
    - add in no-js version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./index.scss";
 import tabHeader from './components/tabHeader/tabHeader';
 
+document.querySelectorAll('.no-js').forEach((element) => {
+    element.style = '';
+    element.classList.remove('no-js');
+});
+
 new tabHeader();
